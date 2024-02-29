df7e78e9508a25c7c1e99153384c236e24117652
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-29 04:26:04 -0400

    Modificación de auth.ts para uso de las rutas de autenticación de Google y Facebook. Actualización de .gitignore

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,7 +4,10 @@ import {
   profile,
   signin,
   signup,
+  googleCallback,
+  facebookCallback,
 } from '../controllers/authentication/auth.controller';
+import passport from 'passport';
 
 const router: Router = Router();
 
@@ -13,5 +16,35 @@ router.post('/signup', signup);
 router.post('/signin', signin);
 
 router.get('/profile', TokenValidation, profile);
+
+// Google Authentication Routes...
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login',
+  }),
+  googleCallback,
+);
+
+// Facebook Authentication Routes...
+router.get(
+  '/facebook',
+  passport.authenticate('facebook', {
+    scope: ['email'],
+  }),
+);
+router.get(
+  'facebook/callback',
+  passport.authenticate('facebook', {
+    failureRedirect: '/login',
+  }),
+  facebookCallback,
+);
 
 export default router;
